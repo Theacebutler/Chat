9e340f85dcc9f54b5a7f3ed75b32049b6dc3940c
Theacebutler <Theacebutler@gmail.com>
2025-09-09 19:25:37 -0400

    added home view

--- Chat/urls.py
+++ Chat/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from main import views
+
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', views.HomeView.as_view(), name='home'),
     path('main/', include('main.urls')),
 ]
