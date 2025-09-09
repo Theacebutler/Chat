f9b0f0bc74b931e2b4dca12b6cffa8bd265b5149
Theacebutler <Theacebutler@gmail.com>
2025-09-09 19:26:15 -0400

    api urls created

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views as v
 urlpatterns = [
-    path('message/new', v.MsgFormView.as_view(), name='form_create')
+    path('groups/all', v.get_groups, name='groups_all'),
+    path('chats/all', v.GetMessages.as_view(), name='chats_all'),
+    path('', v.MainPageView.as_view(), name='main'),
 ]
